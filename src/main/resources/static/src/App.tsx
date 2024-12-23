121a490cb6c856dd8e56828c050dd01578e7b2a0
Charles Bouvier <charlesbouvier832@hotmail.com>
2024-12-23 03:20:13 +0100

    Feat : 404 page when no route match

--- src/main/resources/static/src/App.tsx
+++ src/main/resources/static/src/App.tsx
@@ -1,18 +1,14 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignupPage from "./pages/SignUpPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/signup" element={<SignupPage />} />
-        <Route path="*" element={<Navigate to="/signup" />} />{" "}
+        <Route path="*" element={<NotFoundPage />} />{" "}
         {/* Redirect to signup page */}
       </Routes>
     </Router>
---
+++ src/main/resources/static/src/pages/NotFoundPage.tsx
@@ -0,0 +1,10 @@
+const NotFoundPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Erreur</h1>
+      <p>Page non trouvée</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
